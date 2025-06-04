a3592d25b285872f343914a71decba8d14d30a34
KasihImmanuela <immanuelasamosir31@gmail.com>
2025-06-04 10:07:49 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,14 +32,14 @@ return [
     'public' => [
         'driver' => 'local',
         'root' => storage_path('app/public'),
-        'url' => env('APP_URL').'/storage/app/public',
+        'url' => env('APP_URL').'/storage',
         'visibility' => 'public',
     ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/storage/app/public',
             'visibility' => 'public',
             'throw' => false,
         ],
