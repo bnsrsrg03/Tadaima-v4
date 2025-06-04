354b6bb2449b9fab731bddb9541f568296ece017
KasihImmanuela <immanuelasamosir31@gmail.com>
2025-06-04 10:03:02 +0700

    123

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -11,7 +11,7 @@
     <div class="gallery-grid">
         @foreach ($galeris as $galeri)
             <div class="gallery-item">
-                <img src="{{ asset('storage/' . $galeri->image) }}" alt="Galeri">
+                <img src="{{ asset('storage/app/public/' . $galeri->image) }}" alt="Galeri">
             </div>
         @endforeach
     </div>
